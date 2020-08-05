9a776eb424afcfbe9f2ad1550506b0e82c8c5b6c
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-08-05 20:33:40 -0300

    Carregando font personalizada e header do componente tasklist

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import TaskList from "./src/screens/TaskList";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Lato: require("./assets/fonts/Lato.ttf"),
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <TaskList />;
+  }
+}
